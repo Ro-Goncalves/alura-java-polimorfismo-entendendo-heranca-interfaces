57c624e6d989aa1604ecb4d5cd3b4290d0f8d20a
Rodrigo Gonçalves  - BR Consórcios - Sistemas <rodrigo.goncalves@consorciouniaoldb.com.br>
2022-06-27 13:48:38 -0300

    Fim da aula: 'Começando com a herança'

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Java Polimorfismo: Entenda Herança E Interfaces
 
-Esse é o meu terceiro curso em Java, estou estudando a formação java E Orientação A Objetos.
+Esse é o meu terceiro curso em Java, estou estudando a formação Java E Orientação A Objetos.
 
 * Link da formação: https://www.alura.com.br/formacao-java
 * Link do curso: https://www.alura.com.br/curso-online-java-heranca-interfaces-polimorfismo
+
+Leituras recomendadas:
+* Robert C Martin - Clean Code
---
+++ src/TesteGerente.java
@@ -0,0 +1,22 @@
+
+public class TesteGerente {
+
+	public static void main(String[] args) {
+		Gerente gerente = new Gerente();
+		
+		gerente.setNome("Rodrigo Gonçalves");
+		gerente.setCpf("123.123.123-12");
+		gerente.setSalario(5000);
+		
+		System.out.println(gerente.getNome());
+		
+		boolean autentica = gerente.autentica(222);		
+		System.out.println(autentica);
+		
+		gerente.setSenha(222);
+		autentica = gerente.autentica(222);		
+		System.out.println(autentica);
+
+	}
+
+}
