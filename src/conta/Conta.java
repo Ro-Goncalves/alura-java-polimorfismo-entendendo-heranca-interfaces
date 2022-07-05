b1ab47cf898e4acc85ce3a39022eb4fefad9141e
Rodrigo Gonçalves  - BR Consórcios - Sistemas <rodrigo.goncalves@consorciouniaoldb.com.br>
2022-07-05 13:48:24 -0300

    Fim da aula: Conta Corrente e Poupança

--- src/conta/Conta.java
+++ src/conta/Conta.java
@@ -9,12 +9,9 @@ public class Conta {
     private static int total = 0;
         
     public Conta(int agencia, int numero){
-        Conta.total++;
-        System.out.println("O total de contas é " + Conta.total);
+        Conta.total++;        
         this.agencia = agencia;
-        this.numero = numero;
-        this.saldo = 100;
-        System.out.println("Estou criando uma conta " + this.numero);
+        this.numero = numero;           
     }
 
     public void deposita(double valor) {
